3a89134a80c68913340d7420740261580163cea6
Sam Smedinghoff <smeds1@users.noreply.github.com>
2018-05-09 08:39:21 -0400

    Updated from Brython Server: 5/9/2018 8:39:21 AM

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,13 @@
+#Sam Smedinghoff
+#5/9/18
+#fileDemo.py - how to read a file
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    if 'tso' in line:
+        print(line.strip())
+    numWords += 1
+    
+print(numWords)

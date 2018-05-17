509477752749c28eb5f536c2509c5da724b1c5c0
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-05-17 08:15:35 -0400

    Updated from Brython Server: 5/17/2018 8:15:37 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,12 @@
+#Matthew Suriaiwnata
+#5/17/18
+#warmup17.py
 
+
+
+file = open('engmix.txt')
+
+for line in file:
+    words = line.strip()
+    if "m" in words and "a" in words and "t" in words and "h" in words and "e" in words and "w" in words:
+        print(words)

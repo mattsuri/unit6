bbeb0bfef5e581eb1ffa707ea95df9c3e0c3826c
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-05-21 08:05:48 -0400

    Updated from Brython Server: 5/21/2018 8:05:50 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,15 @@
+#Matthew Suriawinata
+#5/21/18
+#quiz6.py
+
+
+dictionary = open("engmix.txt")
+
+def program1():
+    letter = input("Enter a letter: ")
+    for words in dictionary:
+        letterCount = words.count(letter)
+        if letterCount == 4:
+            print(words.strip())
+program1()
 

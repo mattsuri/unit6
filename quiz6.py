9a1f1d09207e111c3f8555e1d13dcc820c8dcb43
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-05-21 08:21:46 -0400

    Updated from Brython Server: 5/21/2018 8:21:48 AM

--- quiz6.py
+++ quiz6.py
@@ -5,6 +5,7 @@
 
 dictionary = open("engmix.txt")
 
+
 def program1():
     letter = input("Enter a letter: ")
     for words in dictionary:
@@ -13,3 +14,43 @@ def program1():
             print(words.strip())
 program1()
 
+
+
+
+
+def program3():
+    letter = input("Enter a letter: ")
+    num = int(input("Enter a number: "))
+    for words in dictionary:
+        letterCount = words.count(letter)
+        length = len(words)
+        if letterCount == 4 and length == num:
+            print(words.strip())
+program3()
+
+
+def program4():
+    list10 = []
+    for words in dictionary:
+        length = len(words)
+        if length >= 10:
+            list10.append(words)
+    print(list10[8000])
+program4()
+
+
+def program5():
+    vowelCount = 0 
+    lastCount = 0
+    vowelWord = ""
+    for words in dictionary:
+        vowelCount += words.count("a")
+        vowelCount += words.count("e")
+        vowelCount += words.count("i")
+        vowelCount += words.count("o")
+        vowelCount += words.count("u")
+        if vowelCount > lastCount:
+            vowelCount = lastCount
+            vowelWord = words
+    print(vowelWord)
+program5()
